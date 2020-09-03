deaf51c885a32f244e9bc1cf5348c3c19bda9414
ErkkiTa <erkkitamb5@gmail.com>
2020-09-03 11:34:26 +0300

    tt

--- book.php
+++ book.php
@@ -2,8 +2,13 @@
 
 require_once 'models/Book.php';
 
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
-$book = Book::findById(3);
+$book = Book::findById($id);
 
 
 var_dump($book);
+
+?>
+
+<h1><?php echo $book->title; ?></h1>
