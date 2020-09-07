c1d4dbc5579092be4802ba59d3ac0586ec0fe8b4
ErkkiT <erkkitamb5@gmail.com>
2020-09-07 13:23:22 +0300

    tere talv

--- models/Book.php
+++ models/Book.php
@@ -16,6 +16,18 @@ class Book {
 
     }
 
+    public static function findAll() {
+
+        global $pdo;
+
+        $stmt = $pdo->prepare('SELECT * FROM books');
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Book');
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+
+    }
+
 }
 
 
